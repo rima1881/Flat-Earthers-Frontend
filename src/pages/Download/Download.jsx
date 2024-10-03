a5cbada7e612b042d23e9cf8d313a2432600973b
genericlearner <umar.akahyd@gmail.com>
2024-10-03 19:12:59 -0400

    download page(needs css)

---
+++ src/pages/Download/Download.jsx
@@ -0,0 +1,54 @@
+import React, {useEffect} from 'react'
+import Cookies from 'js-cookie'
+import { useTarget } from '../../utils/useTarget'
+
+
+export default function Download(){
+    //Get the targets data from targetstate(from use target)
+    const { targetsState } = useTarget()
+    const { targets } = targetsState()
+   
+    
+    //Download performs the downloading of the csv
+    const download = (data) =>{
+        if(data.length === 0){
+            alert('Add Target points to Download data')
+            return;
+        }
+        
+    //setting up the data for headers and row data
+    const header = ['Latitude', 'Longitude','Path', 'Row', 'Prediction']
+    const rows = data.map(entry =>[
+        entry.lat,
+        entry.lng,
+        entry.path,
+        entry.row,
+        entry.prediction
+    ])
+    //mapping the rows to the headers
+    const csv= [
+        header.join(','),
+        ...rows.map(e => e.join(','))
+    ].join('\n');
+
+    //create a binary large object? with the data and type(csv)
+    const blob = new Blob([csv], {type: 'text/csv;charset=utf-8;' });
+    //creating link and url for the blob and download
+    const link = document.createElement('a');
+    const url = URL.createObjectURL(blob);
+    //link setup for download
+    link.setAttribute('href', url);
+    link.setAttribute('download', 'data.csv');
+    link.style.visibility = 'hidden';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+};
+    const handleDownload = ()=>{
+        console.log(targets)
+        download(targets);
+    }
+    return(
+        <button onClick={handleDownload}>Amir is gay</button>
+    )
+}
