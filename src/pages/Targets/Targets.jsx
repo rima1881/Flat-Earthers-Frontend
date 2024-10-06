28d4eb8e903edf016a884d89dbeb6681dcf938a9
genericlearner <umar.akahyd@gmail.com>
2024-10-06 14:13:38 -0400

    annoying console log

--- src/pages/Targets/Targets.jsx
+++ src/pages/Targets/Targets.jsx
@@ -1,94 +1,72 @@
-import { useTarget } from "../../utils/useTarget"
-import useAPI from "../../utils/useAPI"
-import styles from "./Targets.module.css"
-import useAuth from "../../utils/useAuth"
+import { useTarget } from "../../utils/useTarget";
+import useAPI from "../../utils/useAPI";
+import styles from "./Targets.module.css";
+import useAuth from "../../utils/useAuth";
 
+export default function Targets() {
+  const { targetsState } = useTarget();
+  const { targets, deleteTarget } = targetsState();
+  const { deleteTargetServer } = useAPI();
 
-export default function Targets(){
+  const { userState } = useAuth();
+  const { user } = userState();
 
-    const { targetsState } = useTarget()
-    const { targets, deleteTarget } = targetsState()
-    const { deleteTargetServer } = useAPI()
+  const hasLoggedIn = user.token != "";
 
-    const { userState } = useAuth()
-    const { user } = userState()
+  const deleteHandle = (uuid, index) => {
+    //  Delete local targets
+    deleteTarget(index);
 
-    const hasLoggedIn = user.token != ''
-    console.log(targets)
+    //  Delete server targets
+    if (hasLoggedIn && uuid != -1) deleteTargetServer(uuid);
+  };
 
-    const deleteHandle = (uuid , index) => {
+  const TargetTemplate = ({ guid, index, row, path, lat, lng }) => (
+    <tr>
+      <td>{row}</td>
 
-        //  Delete local targets
-        deleteTarget(index)
+      <td>{path}</td>
+      <td>{lat}</td>
+      <td>{lng}</td>
+      <td>
+        <a href={"examine/" + guid}>Examine</a>
+        <button onClick={() => deleteHandle(guid, index)}>Delete</button>
+      </td>
+    </tr>
+  );
 
-        //  Delete server targets
-        if (hasLoggedIn && uuid != -1)
-            deleteTargetServer(uuid)
-    }
+  const renderedTargets = targets.map((target, index) => (
+    <TargetTemplate
+      guid={target.guid}
+      row={target.row}
+      path={target.path}
+      lat={target.lat}
+      lng={target.lng}
+      key={index}
+      index={index}
+    />
+  ));
 
-    const TargetTemplate = ({guid, index, row, path, lat, lng}) => (
-         
-        <tr>
-            <td>
-                {row}
-            </td>
-           
-            <td>
-                {path}
-            </td>
-            <td>
-                {lat}
-                
-            </td>
-            <td>
-                {lng}
-            </td>
-            <td>
-                <a href={"examine/"+ guid} >Examine</a>
-                <button onClick={() => deleteHandle(guid, index)}>Delete</button>
-            </td>
-        </tr>
-    )
-    
-    const renderedTargets = targets.map( (target, index) => 
-        <TargetTemplate guid={target.guid} row={target.row} path={target.path} lat={target.lat} lng={target.lng} key={index} index={index} /> 
-    
-    )
-
-    return(
-        <div className={styles.container}>
-            <div className={styles.header}>
-                <span>Selected Targets</span>
-                <span>{user.email}</span>
-            </div>
-            <div className={styles.body}>
-                <table>
-                    <thead>
-                    <tr>
-                        <th>
-                            Row
-                        </th>
-                        <th>
-                            Path
-                        </th>
-                        <th>
-                            Latitude
-                        </th>
-                        <th>
-                            Longitude
-                        </th>
-                        <th>
-                            Action
-                        </th>
-                        
-                    </tr>
-                    </thead>
-                    <tbody>
-                        {renderedTargets}
-                    </tbody>
-                </table>
-            </div>
-
-        </div>
-    )
+  return (
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <span>Selected Targets</span>
+        <span>{user.email}</span>
+      </div>
+      <div className={styles.body}>
+        <table>
+          <thead>
+            <tr>
+              <th>Row</th>
+              <th>Path</th>
+              <th>Latitude</th>
+              <th>Longitude</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>{renderedTargets}</tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
